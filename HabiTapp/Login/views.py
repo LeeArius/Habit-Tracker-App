9af03b88b7c1e6db22a641eaf91e975f666fa2e0
Finn_ <leearius.erana@cvsu.edu.ph>
2022-07-29 14:58:50 +0800

    WebApp Updates

--- HabiTapp/Login/views.py
+++ HabiTapp/Login/views.py
@@ -11,6 +11,23 @@ def HomePage(request):
 def LoginPage(request):
     return render(request, 'Login Page.html')
 
+def SignUp(request):
+    return render(request, 'SignUp Page.html')
+
 def Dashboard(request):
     return render(request, 'Dashboard/Dashboard.html')
+
+def WD(request):
+    return render(request, 'Dashboard/Wind Down.html')
+
+def WD2(request):
+    return render(request, 'Dashboard/Wind Down2.html')
+
+def FM(request):
+    return render(request, 'Dashboard/Focus Mode.html')
+
+def FM2(request):
+    return render(request, 'Dashboard/Focus Mode2.html')
     
+def MUA(request):
+    return render(request, 'Dashboard/MUA.html')
